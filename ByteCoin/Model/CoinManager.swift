e22b02b5e1499517bcf8c0d3b44f5a322a96cd31
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-03 18:04:53 +0300

    updated Model to use URLSession and JSON parsing

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,14 +8,48 @@
 
 import Foundation
 
+struct CoinData: Codable {
+    let rate: Double
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "82F3230A-3F3F-45CF-8B5F-2222CFE5B1B4"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+    
+    private func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    return
+                }
+                
+                if let safeData = data {
+                    let rate = parseJSON(safeData)
+                    print(String(format: "%.2f", rate ?? 0.00))
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    private func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
         
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let rate = decodedData.rate
+            return rate
+        } catch {
+            return nil
+        }
     }
 }
